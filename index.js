3fc935904a6457de6e6e04696485296bee92100d
Ramadani <email.ramadani@gmail.com>
2017-09-29 20:03:19 +0700

    install dotenv and setup webhook route

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -9,10 +11,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /*
 Const Data Facebook Detail Apps
 */
-const APP_SECRET    = "APP_SECRET_ANDA";
-const VALID_TOKEN   = "VALID TOKEN ANDA";
-const SERVER_URL    = "https://c5b507c2.ngrok.io";
-const ACCESS_TOKEN  = "PAGE ACCESS TOKEN";
+const APP_SECRET    = process.env.APP_SECRET;
+const VALID_TOKEN   = process.env.VALID_TOKEN;
+const SERVER_URL    = process.env.SERVER_URL;
+const ACCESS_TOKEN  = process.env.ACCESS_TOKEN;
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
@@ -20,4 +22,12 @@ const server = app.listen(process.env.PORT || 5000, () => {
 app.get('/', (req, res) => {
   console.log('Server Ok!');
   res.sendStatus(200);
+});
+
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === VALID_TOKEN) {
+      res.status(200).send(req.query['hub.challenge']);
+  } else {
+      res.status(403).end();
+  }
 });
